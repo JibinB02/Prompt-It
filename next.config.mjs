fd95de2bd3f8c6728251095163c25829ecbf405b
JibinB02 <jibinbabukp02@gmail.com>
2024-05-13 02:18:43 +0530

    first one

---
+++ next.config.mjs
@@ -0,0 +1,34 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        remotePatterns:[
+            {
+                protocol: 'https',
+                hostname:'lh3.googleusercontent.com',
+                port:''
+            }
+        ]
+    }
+};
+
+export default nextConfig;
+
+// const nextConfig ={
+//     experimental:{
+//         appDir:true,
+//         serverComponentsExternalPackages:['mongoose'],
+//     },
+//     images:{
+//         domains:['lh3.googleusercontent.com'],
+//     },
+//     webpack(config) {
+//         config.experiments={
+//             ...config.experiments,
+//             topLevelAwait:true,
+//         }
+//         return config
+//     }
+// }
+
+// export default nextConfig
+
